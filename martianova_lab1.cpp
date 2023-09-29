f1a1fec8d77d6ba89a4042a2d75f6512afe3633b
Svetlana Martianova <smartianova8@mail.ru>
2023-09-29 12:22:56 +0300

    fix: reading and writing to a file

--- martianova_lab1.cpp
+++ martianova_lab1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 
 using namespace std;
 struct Pipe{
@@ -29,6 +30,21 @@ Pipe InputPipe(){
     cin >> pipe.sign;
     return pipe;
 };
+
+//чтение данных из файла о станции
+Station LoadStation(){
+    ifstream fin;
+    // окрываем файл для чтения
+    fin.open("data.txt");
+    Station station;
+    fin >> station.name;
+    fin >> station.countAll;
+    fin >> station.countWork;
+    fin >> station.effect;
+    fin.close();
+    return station;
+};
+
 Station InputStation(){
     Station station;
     cout << "Type name: ";
@@ -41,23 +57,44 @@ Station InputStation(){
     cin >> station.effect;
     return station;
 };
-void PrintPipe(Pipe p){
+
+// обновление значения трубы, передача данных по ссылке
+void EditPipe(Pipe & pipe){
+
+}
+void PrintPipe(const Pipe& p){
     cout << "Name: " << p.name
     << "\tLength: " << p.length
     << "\tDiameter: " << p.diameter
     << "\tSign: " << p.sign<< endl;
 };
-void PrintStation(Station s){
+
+void PrintStation(const Station& s){
     cout << "Name: " << s.name
     << "\tCountAll: " << s.countAll
     << "\tCountWork: " << s.countWork
     << "\tEffectiveness: " << s.effect<< endl;
 };
+
+//сохранение данных о станции в файл
+void SaveStation(const Station& s){
+    //объект файлового потока вывода
+    ofstream fout;
+    //запись в файл
+    fout.open("data.txt");
+    fout  << s.name << endl
+    << s.countAll <<endl
+    << s.countWork << endl
+    << s.effect<< endl;
+    fout.close();
+};
 int main()
 {
     Pipe pipe = InputPipe();
-    PrintPipe(pipe);
-    Station station=InputStation();
-    PrintStation(station);
+    //PrintPipe(pipe);
+    //Station station=InputStation();
+    //PrintStation(station);
+    //SaveStation(station);
+    PrintStation(LoadStation());
     return 0;
 }
