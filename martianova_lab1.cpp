fe7e1975dc186a34f7b92f2305878c54dc629af0
Svetlana Martianova <smartianova8@mail.ru>
2023-10-01 11:43:38 +0300

    fix:  & 'c:\Users\user\.vscode\extensions\ms-vscode.cpptools-1.17.5-win32-x64\debugAdapters\bin\WindowsDebugLauncher.exe' '--stdin=Microsoft-MIEngine-In-slhabtal.npy' '--stdout=Microsoft-MIEngine-Out-mcpnvqzs.10b' '--stderr=Microsoft-MIEngine-Error-a5nkshny.mel' '--pid=Microsoft-MIEngine-Pid-31m2s0dq.bgp' '--dbgExe=C:\msys64\mingw64\bin\gdb.exe' '--interpreter=mi'

--- martianova_lab1.cpp
+++ martianova_lab1.cpp
@@ -2,13 +2,15 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <cstring>
 
 using namespace std;
 struct Pipe{
     string name;
     double length;
     double diameter;
-    string sign;
+    bool isRepair;
 };
 
 struct Station
@@ -18,57 +20,180 @@ struct Station
     int countWork;
     string effect;
 };
-Pipe InputPipe(){
+//функция ввода трубы
+Pipe InputPipe(bool & isPipe){
     Pipe pipe;
-    cout << "Type name: ";
-    cin >> pipe.name;
-    cout << "Type length: ";
-    cin >> pipe.length;
-    cout << "Type diameter: ";
-    cin >> pipe.diameter;
-    cout << "Type sign: ";
-    cin >> pipe.sign;
+    do{
+        //очищает флаг ошибки
+        cin.clear();
+        //переходит к следующей новой строке
+        cin.ignore(10000, '\n');
+        cout << "Type name: ";      
+    getline(cin, pipe.name);
+    }while(pipe.name=="");
+        do {
+            if (cin.fail()){
+                cin.clear();
+                cin.ignore(10000, '\n');
+            }
+            cout << "Type length: ";
+            cin >> pipe.length;
+        }while(cin.fail());
+        do {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Type diameter: ";
+            cin >> pipe.diameter;
+        }while(cin.fail());
+        do {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Type in repair (1=in repair, 0=not in repair): ";
+            cin >> pipe.isRepair;
+        }while(cin.fail());
+    isPipe=true;
     return pipe;
+};
+
+//редактирование трубы
+void EditPipe(Pipe & pipe){
+    if (pipe.isRepair==1){
+        pipe.isRepair=0;
+    }
+    else{
+        pipe.isRepair=1;
+    }
+};
+
+//редактирование станции
+void EditStation(Station & station){
+    do{
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cout << "Number of workshops in operation: ";
+        cin >> station.countWork;
+    }while(cin.fail());
+};
+
+//вывод трубы в консоль
+void PrintPipe(const Pipe& p){
+    cout << "Name: " << p.name
+    << "\tLength: " << p.length
+    << "\tDiameter: " << p.diameter
+    << "\tIsRepair: " << p.isRepair<< endl;
 };
 
 //чтение данных из файла о станции
 Station LoadStation(){
     ifstream fin;
-    // окрываем файл для чтения
+    string st, s;
+    Station station;
+    string a1[4];
+    int index=0;
+    // окрываем файл
     fin.open("data.txt");
-    Station station;
-    fin >> station.name;
-    fin >> station.countAll;
-    fin >> station.countWork;
-    fin >> station.effect;
-    fin.close();
+    //проверка на открытие файла
+    if (fin.is_open()){
+        while(getline(fin, st)){ // пока не достигнут конец файла класть очередную строку в переменную (st)
+        //ss - это объект stringstream, который ссылается на строку st.
+        stringstream ss(st);
+        if (st.find("station")!= string::npos){
+        while ( getline( ss, s, ',' ) ) {
+            if (index==0){
+                // c_str () возвращает указатель на символьный массив, который содержит строку объекта стринг (string) в том виде, в котором она размещалась бы, во встроенном строковом типе
+                a1[index]=s.substr(9,strlen(s.c_str()));
+            }
+            else{
+                a1[index]=s;
+            }
+            index+=1;
+            
+  }
+    station.name=a1[0];
+    //преобрзование строки в число
+    station.countAll=stoi(a1[1]);
+    station.countWork=stoi(a1[2]);
+    station.effect=a1[3];
+        }
+    }
+        fin.close();
+    }
+    
     return station;
 };
 
-Station InputStation(){
+//чтение данных из файла о трубе
+Pipe LoadPipe(){
+    ifstream fin;
+    string st, s;
+    Pipe pipe;
+    string a1[4];
+    int index=0;
+    // окрываем файл
+    fin.open("data.txt");
+    //проверка на открытие файла
+    if (fin.is_open()){
+        while(getline(fin, st)){ // пока не достигнут конец файла класть очередную строку в переменную (st)
+        //ss - это объект stringstream, который ссылается на строку st.
+        stringstream ss(st);
+        if (st.find("pipe")!= string::npos){
+        while ( getline( ss, s, ',' ) ) {
+            if (index==0){
+                // c_str () возвращает указатель на символьный массив, который содержит строку объекта стринг (string) в том виде, в котором она размещалась бы, во встроенном строковом типе
+                a1[index]=s.substr(6,strlen(s.c_str()));
+            }
+            else{
+                a1[index]=s;
+            }
+            index+=1;
+            
+  }
+    pipe.name=a1[0];
+    //преобрзование строки в число
+    pipe.length=stoi(a1[1]);
+    pipe.diameter=stoi(a1[2]);
+    pipe.isRepair=stoi(a1[3]);
+        }
+    }
+        fin.close();
+    }
+    
+    return pipe;
+};
+//функция ввода станции
+Station InputStation(bool & isStation){
     Station station;
+    do{
+    cin.clear();
+    cin.ignore(10000, '\n');
     cout << "Type name: ";
-    cin >> station.name;
-    cout << "Number of workshops: ";
-    cin >> station.countAll;
-    cout << "Number of workshops in operation: ";
-    cin >> station.countWork;
-    cout << "Type effectiveness: ";
-    cin >> station.effect;
+    getline(cin, station.name);
+    }while(station.name=="");
+    do{
+        if (cin.fail()){
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+        cout << "Number of workshops: ";
+        cin >> station.countAll;
+    }while(cin.fail());
+    do{
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cout << "Number of workshops in operation: ";
+        cin >> station.countWork;
+    }while(cin.fail());
+    do{
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cout << "Type effectiveness: ";
+        cin >> station.effect;
+    }while(cin.fail());
+    isStation=true;
     return station;
 };
 
-// обновление значения трубы, передача данных по ссылке
-void EditPipe(Pipe & pipe){
-
-}
-void PrintPipe(const Pipe& p){
-    cout << "Name: " << p.name
-    << "\tLength: " << p.length
-    << "\tDiameter: " << p.diameter
-    << "\tSign: " << p.sign<< endl;
-};
-
+// обновление значения станции, передача данных по ссылке
 void PrintStation(const Station& s){
     cout << "Name: " << s.name
     << "\tCountAll: " << s.countAll
@@ -76,25 +201,109 @@ void PrintStation(const Station& s){
     << "\tEffectiveness: " << s.effect<< endl;
 };
 
-//сохранение данных о станции в файл
-void SaveStation(const Station& s){
+//сохранение данных в файл
+void SaveStationPipe(const Station& s, const Pipe p, bool isStation, bool isPipe){
     //объект файлового потока вывода
     ofstream fout;
     //запись в файл
-    fout.open("data.txt");
-    fout  << s.name << endl
-    << s.countAll <<endl
-    << s.countWork << endl
-    << s.effect<< endl;
-    fout.close();
+    fout.open("data.txt", ios::out);
+    if (fout.is_open()){
+        if (isStation){
+        fout  <<"station:"<< ' '<< s.name<< "," << to_string(s.countAll)<< "," << to_string(s.countWork)<< ","<<s.effect<< endl;
+        }
+        if (isPipe){
+            fout  <<"pipe:"<< ' '<< p.name<< "," << to_string(p.length)<< "," << to_string(p.diameter)<< ","<<to_string(p.isRepair)<< endl;
+        }
+        fout.close();
+    } 
 };
+
+void PrintMenu(){
+    cout << "Добавить трубу: 1" << endl
+        << "Добавить КС: 2" << endl
+        << "Просмотр всех объектов: 3" << endl
+        << "Редактировать трубу: 4" << endl
+        << "Редактировать КС: 5" << endl
+        << "Сохранить: 6" << endl
+        << "Загрузить: 7" << endl
+        << "Выход: 0" << endl;
+};
+
 int main()
 {
-    Pipe pipe = InputPipe();
-    //PrintPipe(pipe);
-    //Station station=InputStation();
-    //PrintStation(station);
-    //SaveStation(station);
-    PrintStation(LoadStation());
+    Station station;
+    Pipe pipe;
+    bool isStation=false;
+    bool isPipe=false;
+    while(1){
+        PrintMenu();
+        int i = 0;
+        cin >> i;
+        switch (i)
+        {
+        case 1:
+        {
+            pipe=InputPipe(isPipe);
+            break;
+        }
+        case 2:
+        {
+            station=InputStation(isStation);
+            break;
+        }
+        case 3:
+        {
+            if (isStation){
+                cout << "Station" << endl;
+                PrintStation(station);
+            }
+            if (isPipe){
+                cout << "Pipe" << endl;
+                PrintPipe(pipe);
+            }
+            break;
+        }
+        case 4:
+        {
+            if (isPipe){
+                EditPipe(pipe);
+                PrintPipe(pipe);
+            }
+            else{
+                cout << "Трубы не существует"<<endl;
+            }
+            break;
+        }
+        case 5:
+        {
+            if (isStation){
+                EditStation(station);
+                PrintStation(station);
+            }
+            else{
+                cout << "Станции не существует"<<endl;
+            }
+            break;
+        }
+        case 6:
+        {
+            SaveStationPipe(station,pipe, isStation, isPipe);
+            break;
+        }
+        case 7:
+        {
+            PrintStation(LoadStation());
+            PrintPipe(LoadPipe());
+            break;
+        }
+        case 0:
+        {
+            return 0;
+        }
+        default:{
+            cout << "Произошла ошибка" << endl;
+        }
+        }
+    }
     return 0;
 }
